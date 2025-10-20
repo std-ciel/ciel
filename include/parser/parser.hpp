38ae346a5b9c92b6ac78f9758f4c705de332df77
vibhatsu <maulikbarot2915@gmail.com>
2025-10-20 22:18:36 +0530

    refactor(parser): remove setter functions for parsed data
    Signed-off-by: vibhatsu <maulikbarot2915@gmail.com>

--- include/parser/parser.hpp
+++ include/parser/parser.hpp
@@ -14,9 +14,4 @@ std::vector<ASTNodePtr> get_parsed_translation_unit();
 SymbolTable &get_symbol_table();
 TypeFactory &get_type_factory();
 
-// Setter functions (called by parser)
-void set_parsed_translation_unit(const std::vector<ASTNodePtr> &tu);
-void set_symbol_table(SymbolTable *st);
-void set_type_factory(TypeFactory *tf);
-
 #endif // PARSER_HPP
